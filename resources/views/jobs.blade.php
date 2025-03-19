a265bec39b35fde1fadaa1a18e00bbecf2214947
RothaSAMON <samonrotha@gmail.com>
2025-03-19 15:51:47 +0700

    7d - Autoloading, Namespace, and Models

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+{{-- <x-layout>
   <x-slot:heading>
     Job Listings
   </x-slot:heading>
@@ -12,4 +12,22 @@
     </li>
     @endforeach
   </ul>
+</x-layout> --}}
+
+<x-layout>
+  <x-slot:heading>
+      Job Listings
+  </x-slot:heading>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      @foreach ($jobs as $job)
+          <div class="bg-white shadow-md rounded-lg p-6 border border-gray-200">
+              <h3 class="text-lg font-semibold text-blue-600">
+                  <a href="/jobs/{{ $job['id'] }}" class="hover:underline">{{ $job['title'] }}</a>
+              </h3>
+              <p class="text-gray-700 mt-2">Pays {{ $job['salary'] }} per year.</p>
+          </div>
+      @endforeach
+  </div>
 </x-layout>
+
