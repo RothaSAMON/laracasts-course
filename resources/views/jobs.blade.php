38e8d3a4422d8268ad465076696d3d8547714eab
RothaSAMON <samonrotha@gmail.com>
2025-03-20 15:27:24 +0700

    13d - Eager Loading and the N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -22,10 +22,12 @@
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       @foreach ($jobs as $job)
           <div class="bg-white shadow-md rounded-lg p-6 border border-gray-200">
+              <p class="font-bold">{{ $job->employer->name }}</p>
+
               <h3 class="text-lg font-semibold text-blue-600">
                   <a href="/jobs/{{ $job['id'] }}" class="hover:underline">{{ $job['title'] }}</a>
               </h3>
-              <p class="text-gray-700 mt-2">Pays {{ $job['salary'] }} per year.</p>
+              <p class="text-gray-500 mt-2">Pays {{ $job['salary'] }} per year.</p>
           </div>
       @endforeach
   </div>
